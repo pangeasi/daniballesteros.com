3d49b85a1fad1ced7d58330684e404d2326ea6af
Dani Ballesteros <pangeanoteniafronteras@gmail.com>
2022-10-11 08:30:18 +0200

    remove chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,8 @@
-import { extendTheme } from "@chakra-ui/react";
 import { AnimatePresence } from "framer-motion";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ToastContainer } from "react-toastify";
 import "../styles/global.css";
 import "react-toastify/dist/ReactToastify.css";
-const theme = extendTheme({
-  fonts: {
-    body: "Montserrat",
-    heading: "Montserrat",
-  },
-});
 
 const MyApp = ({ Component, pageProps }) => {
   const client = new QueryClient();
