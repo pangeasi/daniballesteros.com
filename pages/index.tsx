50fd283cbc6433974502092d696f0e8cb7103659
Dani Ballesteros <pangeanoteniafronteras@gmail.com>
2022-10-12 10:34:12 +0200

    feat: add cv

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,34 +5,13 @@ import { Layout } from "../components/layout/Layout";
 import GithubIcon from "../components/svgs/Github";
 import LinkedinIcon from "../components/svgs/Linkedin";
 import TwitterIcon from "../components/svgs/Twitter";
-//import Projects from "../components/Home/projects";
-// import { ProjectsIcon } from "../components/Icons/projects";
-// import { SkillsIcon } from "../components/Icons/skills";
 
 export default function Home() {
   return (
     <Layout>
       <AboutMe />
-      {/* <section id="projects">
-        <h2 className="text-3xl mt-8">
-          <span>
-            <ProjectsIcon className="text-5xl inline-flex pb-1" />
-          </span>{" "}
-          Proyectos
-        </h2>
-        <Projects />
-      </section> */}
-
-      {/* <section id="skills">
-        <h2 className="text-3xl mt-8">
-          <span>
-            <SkillsIcon className="text-5xl inline-flex pb-1" />
-          </span>{" "}
-          Habilidades
-        </h2>
-      </section> */}
       <section id="rrss">
-        <div className="flex justify-center gap-6">
+        <div className="flex justify-center gap-6 items-center">
           <a
             href="https://www.linkedin.com/in/dani-ballesteros/"
             target="_blank"
@@ -57,6 +36,9 @@ export default function Home() {
           >
             <TwitterIcon className="text-5xl" />
           </a>
+          <a href="https://mnf.red/daniballesteros" target="_blank">
+            <span className="text-[36px] font-bold text-sky-600">CV</span>
+          </a>
         </div>
       </section>
       <section id="contact" className="flex flex-col items-center">
@@ -68,31 +50,6 @@ export default function Home() {
         </h2>
         <Contact />
       </section>
-      {/* <VStack align="start">
-        <HStack>
-          <Text fontWeight="bold" fontSize={{ base: 40 }}>
-            Hola, soy Dani
-          </Text>
-        </HStack>
-
-        <Text fontSize={{ base: 20 }}>
-          Desarrollador front-end, especializado en react
-        </Text>
-      </VStack>
-      <HStack spacing={6} mt={10} justify="end">
-        <Link href="https://github.com/pangeasi" color="purple.400">
-          GitHub
-        </Link>
-        <Link href="https://twitter.com/Danitiwt" color="blue.400">
-          Twitter
-        </Link>
-        <Link
-          href="https://www.linkedin.com/in/dani-ballesteros/"
-          color="green.400"
-        >
-          Linkedin
-        </Link>
-      </HStack> */}
     </Layout>
   );
 }
